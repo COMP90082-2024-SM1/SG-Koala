9e9843683df8c14ab9e32ff02b57b3a5c3ad5486
yongliq <111257540+YongLiMatt@users.noreply.github.com>
2024-03-24 16:23:37 +1100

    initial front-end and back-end

---
+++ launch_fullstack.sh
@@ -0,0 +1,27 @@
+
+#!/bin/bash
+
+# Launch Back-end
+cd back-end
+echo "Activating virtual environment..."
+source myvenv/bin/activate
+echo "Starting Django server..."
+cd Koala_backend
+python manage.py runserver 8000 &
+
+
+sleep 3
+DJANGO_PID=$(lsof -ti:8000)
+
+# Launch front-end
+cd ..
+cd ../front-end/koala_frontend
+echo "Starting React server..."
+npm start &
+REACT_PID=$!
+
+
+echo "Django PID: $DJANGO_PID"
+echo "React PID: $REACT_PID"
+trap 'echo "Stopping servers..."; kill -9 "$DJANGO_PID" "$REACT_PID";
+exit' SIGINT
