2f4d1c8cc9a1ef893243b1c22b10dba3f9eafff0
is0xjh25 <is0.jimhsiao@gmail.com>
2024-05-15 13:56:37 +1000

    FIX small bugs

--- src/front-end/src/pages/Database.js
+++ src/front-end/src/pages/Database.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from "react";
 import Header from "../components/Header/Header";
 import "../styles/Database.css";
 import {
@@ -12,8 +12,17 @@ import { Grouped_Columns } from "../components/Table/Column";
 import { Popover } from "@material-ui/core";
 import ColumnFilter from "../components/Table/ColumnFilter";
 import GlobalFilter from "../components/Table/GlobalFilter";
+import { TypographyParagraph } from "../components/Typography/Typography";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSort, faSortUp, faSortDown, faTableColumns, faBars, faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSort,
+  faSortUp,
+  faSortDown,
+  faTableColumns,
+  faBars,
+  faArrowRight,
+  faArrowLeft,
+} from "@fortawesome/free-solid-svg-icons";
 import { getAllBooking } from "../api/NewbookingAPI";
 
 function Database() {
@@ -30,7 +39,7 @@ function Database() {
       } catch (error) {
         console.error("Data fetch failed:", error);
       }
-    }
+    };
     fetchData();
   }, []);
 
@@ -39,11 +48,13 @@ function Database() {
   const [selectGroup, setSelectGroup] = useState("All");
 
   const groupOptions = useMemo(() => {
-    const options = columns.filter(column => column.columns && column.Header !== ' ').map(column => ({
-      label: column.Header,
-      value: column.Header
-    }));
-    options.unshift({label: 'All', value: 'All'});
+    const options = columns
+      .filter((column) => column.columns && column.Header !== " ")
+      .map((column) => ({
+        label: column.Header,
+        value: column.Header,
+      }));
+    options.unshift({ label: "All", value: "All" });
     return options;
   }, [columns]);
 
@@ -52,19 +63,22 @@ function Database() {
   };
 
   const filteredColumns = useMemo(() => {
-    const baseColumns = [{
-      Header: 'ID',
-      accessor: 'id',
-      canSort: false,
-    }];
-    if (selectGroup === 'All') {
+    const baseColumns = [
+      {
+        Header: "ID",
+        accessor: "id",
+        canSort: false,
+      },
+    ];
+    if (selectGroup === "All") {
       console.log(columns);
       return columns;
-    }
-    else {
-      const cols = columns.find(column => column.Header === selectGroup)?.columns;
+    } else {
+      const cols = columns.find(
+        (column) => column.Header === selectGroup
+      )?.columns;
       console.log(cols);
-      return baseColumns.concat({Header: selectGroup, columns: cols});
+      return baseColumns.concat({ Header: selectGroup, columns: cols });
     }
   }, [selectGroup, columns]);
 
@@ -72,21 +86,24 @@ function Database() {
   const [ishide, setIsHide] = useState(false);
   const [columnIsHovered, setColumnIsHovered] = useState(false);
   const [filterIsHovered, setFilterIsHovered] = useState(false);
-  
+
   const openPopover = (event) => {
     setAnchor(event.currentTarget);
   };
 
   const handleHide = () => {
-    if (ishide) {setIsHide(false)}
-    else {setIsHide(true)}
-  }
+    if (ishide) {
+      setIsHide(false);
+    } else {
+      setIsHide(true);
+    }
+  };
 
   const defaultColumn = useMemo(() => {
     return {
       Filter: ColumnFilter,
-    }
-  }, [])
+    };
+  }, []);
 
   const {
     getTableProps,
@@ -102,133 +119,185 @@ function Database() {
     state,
     setGlobalFilter,
     allColumns,
-  } = useTable({
-    columns: filteredColumns, 
-    data, 
-    defaultColumn,
-    initialState: { sortBy: [] },
-  }, useFilters, useGlobalFilter, useSortBy, usePagination)
+  } = useTable(
+    {
+      columns: filteredColumns,
+      data,
+      defaultColumn,
+      initialState: { sortBy: [] },
+    },
+    useFilters,
+    useGlobalFilter,
+    useSortBy,
+    usePagination
+  );
 
   const { globalFilter, pageIndex } = state;
 
-  const sortIcon = <FontAwesomeIcon icon={faSort} style={{ fontSize: "15px" }} />
-  const sortUp = <FontAwesomeIcon icon={faSortUp} style={{ fontSize: "15px" }} />
-  const sortDown = <FontAwesomeIcon icon={faSortDown} style={{ fontSize: "15px" }} />
-  const next = <FontAwesomeIcon icon={faArrowRight} style={{ fontSize: '15px' }} />
-  const previous = <FontAwesomeIcon icon={faArrowLeft} style={{ fontSize: '15px' }} /> 
+  const sortIcon = (
+    <FontAwesomeIcon icon={faSort} style={{ fontSize: "15px" }} />
+  );
+  const sortUp = (
+    <FontAwesomeIcon icon={faSortUp} style={{ fontSize: "15px" }} />
+  );
+  const sortDown = (
+    <FontAwesomeIcon icon={faSortDown} style={{ fontSize: "15px" }} />
+  );
+  const next = (
+    <FontAwesomeIcon icon={faArrowRight} style={{ fontSize: "15px" }} />
+  );
+  const previous = (
+    <FontAwesomeIcon icon={faArrowLeft} style={{ fontSize: "15px" }} />
+  );
 
   if (loading) {
-    return <div>
-      <Header> Database </Header>
+    return (
       <div>
-        <TypographyParagraph style={ {colorP: 'black'} }>
-          Loading Data...
-        </TypographyParagraph>
+        <Header> Database </Header>
+        <div>
+          <TypographyParagraph style={{ colorP: "black" }}>
+            Loading...
+          </TypographyParagraph>
+        </div>
       </div>
-      </div>;
+    );
   }
   return (
-  <div>
-    <Header> Database </Header>
-    <div className='bg'>
-      <GlobalFilter filter = {globalFilter} setFilter={setGlobalFilter} /> {' '}
-      <select onChange={handleSelectGroup} value={selectGroup}>
-        {groupOptions.map(option => (
-          <option key={option.value} value={option.value}>{option.label}</option>
-        ))}
-      </select>
-      <button onClick={openPopover} 
-              onMouseEnter={()=>setColumnIsHovered(true)} 
-              onMouseLeave={()=>setColumnIsHovered(false)} 
-              className='Button1' > 
-        <FontAwesomeIcon icon={faTableColumns} style={{ fontSize: "15px" }} />
-        {columnIsHovered && <div className='showMessage'>
-          <TypographyParagraph style={{color:'black'}}>
-            Show/Hide columns
-          </TypographyParagraph>
-          </div>}
-      </button>
-      <button onClick={handleHide} 
-              onMouseEnter={()=>setFilterIsHovered(true)} 
-              onMouseLeave={()=>setFilterIsHovered(false)} 
-              className='Button1'>
-        <FontAwesomeIcon icon={faBars} style={{ fontSize: "15px" }} />
-        {filterIsHovered && <div className='showMessage'>
-          <TypographyParagraph style={{color:'black'}}>
-            Show/Hide filters
-          </TypographyParagraph>
-        </div>}
-      </button>
-      <Popover 
-        open={Boolean(anchor)} 
-        anchorEl={anchor} 
-        onClose={() => setAnchor(null)}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left'
-      }}> 
-      
-        <div>
-          {allColumns.map(column => (
-            <div key = {column.id}>
-              <label>
-                <input type='checkBox' {...column.getToggleHiddenProps()} />
-                {column.Header}
-              </label>
+    <div>
+      <Header> Database </Header>
+      <div className="bg">
+        <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />{" "}
+        <select onChange={handleSelectGroup} value={selectGroup}>
+          {groupOptions.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+        <button
+          onClick={openPopover}
+          onMouseEnter={() => setColumnIsHovered(true)}
+          onMouseLeave={() => setColumnIsHovered(false)}
+          className="Button1"
+        >
+          <FontAwesomeIcon icon={faTableColumns} style={{ fontSize: "15px" }} />
+          {columnIsHovered && (
+            <div className="showMessage">
+              <TypographyParagraph style={{ color: "black" }}>
+                Show/Hide columns
+              </TypographyParagraph>
             </div>
-          ))}
-        </div>
-      </Popover>
-    <div className='scrollx'>
-      <table {...getTableProps()} className='table'>
-        <thead>
-          {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              { headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps()} className='th'>
-                  <div>
-                    {column.render('Header')}
-                  {' '}
-                  {!column.disableSortBy && (
-                     <button {...column.getSortByToggleProps()} className='Button1'>
-                      <span>
-                          {column.isSorted ? (column.isSortedDesc ? sortUp : sortDown) : sortIcon}
-                      </span>
-                      </button>
-                  )}
-                  </div>
-                  <div>
-                    {column.canFilter && ishide ? column.render('Filter') : null}
-                  </div>
-                </th>
+          )}
+        </button>
+        <button
+          onClick={handleHide}
+          onMouseEnter={() => setFilterIsHovered(true)}
+          onMouseLeave={() => setFilterIsHovered(false)}
+          className="Button1"
+        >
+          <FontAwesomeIcon icon={faBars} style={{ fontSize: "15px" }} />
+          {filterIsHovered && (
+            <div className="showMessage">
+              <TypographyParagraph style={{ color: "black" }}>
+                Show/Hide filters
+              </TypographyParagraph>
+            </div>
+          )}
+        </button>
+        <Popover
+          open={Boolean(anchor)}
+          anchorEl={anchor}
+          onClose={() => setAnchor(null)}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+        >
+          <div>
+            {allColumns.map((column) => (
+              <div key={column.id}>
+                <label>
+                  <input type="checkBox" {...column.getToggleHiddenProps()} />
+                  {column.Header}
+                </label>
+              </div>
+            ))}
+          </div>
+        </Popover>
+        <div className="scrollx">
+          <table {...getTableProps()} className="table">
+            <thead>
+              {headerGroups.map((headerGroup) => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map((column) => (
+                    <th {...column.getHeaderProps()} className="th">
+                      <div>
+                        {column.render("Header")}{" "}
+                        {!column.disableSortBy && (
+                          <button
+                            {...column.getSortByToggleProps()}
+                            className="Button1"
+                          >
+                            <span>
+                              {column.isSorted
+                                ? column.isSortedDesc
+                                  ? sortUp
+                                  : sortDown
+                                : sortIcon}
+                            </span>
+                          </button>
+                        )}
+                      </div>
+                      <div>
+                        {column.canFilter && ishide
+                          ? column.render("Filter")
+                          : null}
+                      </div>
+                    </th>
+                  ))}
+                </tr>
               ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {page.map((row) => {
-            prepareRow(row)
-            return(
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => {
-                  return <td {...cell.getCellProps()} className='td'>
-                    {cell.render('Cell')}
-                  </td>
-                })}
-          </tr>
-            )
-          })} 
-        </tbody>
-      </table>
-      <div>
-        <button onClick={() => previousPage()} disabled={!canPreviousPage} className='Button2'>{previous}</button>{' '}
-        <span>
-          Page{' '}
-          <strong>
-            {pageIndex + 1} of {pageOptions.length}
-          </strong>
-        </span>
-        <button onClick={() => nextPage()} disabled={!canNextPage} className='Button2'>{next}</button>
+            </thead>
+            <tbody {...getTableBodyProps()}>
+              {page.map((row) => {
+                prepareRow(row);
+                return (
+                  <tr {...row.getRowProps()}>
+                    {row.cells.map((cell) => {
+                      return (
+                        <td {...cell.getCellProps()} className="td">
+                          {cell.render("Cell")}
+                        </td>
+                      );
+                    })}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <div>
+            <button
+              onClick={() => previousPage()}
+              disabled={!canPreviousPage}
+              className="Button2"
+            >
+              {previous}
+            </button>{" "}
+            <span>
+              Page{" "}
+              <strong>
+                {pageIndex + 1} of {pageOptions.length}
+              </strong>
+            </span>
+            <button
+              onClick={() => nextPage()}
+              disabled={!canNextPage}
+              className="Button2"
+            >
+              {next}
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
