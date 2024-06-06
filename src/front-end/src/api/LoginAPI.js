2d220bbd8ed60f6ee6c66b3cec93e9e06c8ac64d
Mor-Poi <xyb774355252@hotmail.com>
2024-06-06 21:05:17 +1000

    UPDATE login api

--- src/front-end/src/api/LoginAPI.js
+++ src/front-end/src/api/LoginAPI.js
@@ -1,28 +1,27 @@
-const baseUrl = "http://localhost:8000/api/login/";
+const baseUrl = process.env.REACT_APP_BASEURL + "login/";
 
 export const LoginDetail = async (username, password) => {
-    try {
-      const response = await fetch(baseUrl, {
-        method: "POST",
-        credentials: "include",
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username,password })
-        })
-      if (!response.ok) {
-        if(response.status === 401) {
-          throw new Error("Invalid Username or Password");
-        } else if(response.status === 400) {
-          throw new Error("Missing Username or Password");
-        }
-        else {
-          throw new Error("Network response was not ok");
-        }
+  try {
+    const response = await fetch(baseUrl, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+    if (!response.ok) {
+      if (response.status === 401) {
+        throw new Error("Invalid Username or Password");
+      } else if (response.status === 400) {
+        throw new Error("Missing Username or Password");
+      } else {
+        throw new Error("Network response was not ok");
       }
-      return await response.json();
-    } catch (error) {
-      console.error("Error fetching login detail:", error);
-      throw error;
     }
-  };
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching login detail:", error);
+    throw error;
+  }
+};
