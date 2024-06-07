28f722e9e7bf0c98bf6eb3566ced4f00b3c2c643
Leona7658 <leona7658@gmail.com>
2024-06-07 14:44:24 +1000

    UPDATE jwt auth settings

--- src/back-end/example/views/views.py
+++ src/back-end/example/views/views.py
@@ -1,13 +1,15 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 import json
 
-
 @api_view(["POST"])
+@permission_classes([AllowAny])
 def login_method(request):
+    
     body_unicode = request.body.decode("utf-8")
     print("Received body:", body_unicode)  # Log the body to debug
 
@@ -30,13 +32,18 @@ def login_method(request):
     if not user.check_password(password):
         return JsonResponse({"error": "Incorrect username or password"}, status=400)
 
-    access_token = AccessToken.for_user(user)
-    refresh_token = RefreshToken.for_user(user)
-    return Response(
-        {
-            "message": "Login successful",
-            "access_token": str(access_token),
-            "refresh_token": str(refresh_token),
-        },
-        status=200,
-    )
+    # Issue tokens
+    try:
+        access_token = AccessToken.for_user(user)
+        refresh_token = RefreshToken.for_user(user)
+        return Response(
+            {
+                "message": "Login successful",
+                "access_token": str(access_token),
+                "refresh_token": str(refresh_token),
+            },
+            status=200,
+        )
+    except Exception as e:
+        return JsonResponse({"error": "Error issuing tokens"}, status=500)
+
